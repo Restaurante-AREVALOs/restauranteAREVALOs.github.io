09e3cbfb31fcd25576daacf3df264ad8492d1424
SeanArevalo <99769829+SeanArevalo@users.noreply.github.com>
2022-03-22 16:50:05 -0600

    Update and rename actionpage.php to action_page.php

---
+++ action_page.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["name"]; ?><br>
+Your apellido address is: <?php echo $_POST["apellidos"]; ?>
+Your telefono address is: <?php echo $_POST["telefono"]; ?>
+Your direccion address is: <?php echo $_POST["direccion"] ?> 
+
+</body>
+</html>  
