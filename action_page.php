5ac7b3f8db931c7158ffb9071a5b6b0f98463c95
SeanArevalo <99769829+SeanArevalo@users.noreply.github.com>
2022-03-22 17:16:48 -0600

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -2,9 +2,10 @@
 <body>
 
 Welcome <?php echo $_POST["name"]; ?><br>
-Your apellido address is: <?php echo $_POST["apellidos"]; ?>
-Your telefono address is: <?php echo $_POST["telefono"]; ?>
-Your direccion address is: <?php echo $_POST["direccion"] ?> 
+Welcome <?php echo $_POST["lname"]; ?><br>
+<?php echo $_POST["apellidos"]; ?>
+<?php echo $_POST["telefono"]; ?>
+<?php echo $_POST["direccion"] ?> 
 
 </body>
 </html>  
